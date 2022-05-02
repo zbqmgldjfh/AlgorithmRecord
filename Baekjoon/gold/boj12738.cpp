d621b55c3b6ec60f22e390b11327b1761da25a98
jiwoo <zbqmgldjfh@gmail.com>
2022-05-03 08:35:18 +0900

    백준 12738번 풀이

---
+++ Baekjoon/gold/boj12738.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 101
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt", "rt", stdin);
+
+    cin >> N;
+
+    vector<int> vec;
+    vec.push_back(INT_MIN);
+    for(int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+
+        if(vec.back() < num) {
+            vec.push_back(num);
+        }else {
+            const vector<int>::iterator &iter = lower_bound(vec.begin(), vec.end(), num);
+            *iter = num;
+        }
+    }
+
+    int len = vec.size();
+    cout << len - 1 << "\n";
+
+    return 0;
+}
+
