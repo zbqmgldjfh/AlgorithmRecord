1735b81ef05e599680dedf38d3cbdfc05b7446c2
jiwoo <zbqmgldjfh@gmail.com>
2022-12-29 20:13:03 +0900

    leetcode 49번

--- leetcode/group_anagrams.cpp
+++ leetcode/group_anagrams.cpp
@@ -1,4 +1,22 @@
-//
-// Created by jiwoo kim on 2022/12/28.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> word_map;
+
+        for (auto word : strs) {
+            string temp = word;
+            sort(temp.begin(), temp.end());
+            word_map[temp].push_back(word);
+        }
+
+        vector<vector<string>> res;
+        for(auto it : word_map) {
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
